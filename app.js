852547a81d786592f08fe70d2c5d1f1cd02e721b
Kate Erickson <59575050+ericksonk6@users.noreply.github.com>
2020-09-16 14:47:10 -0700

    app.js trying to get port to work pt 8 added "tick"

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 const http = require('http');
 
 const port = process.env.PORT || 5000;
-//added
-// const $ = require('jQuery');
-//
-// $("<h1>Include this using jquery</h1>").appendTo("body");
-// console.log($("body").html());
-//added
+
 const server = http.createServer((request, response) => {
     response.statusCode = 200;
     response.setHeader('Content-Type', 'text/plain');
@@ -17,5 +12,5 @@ server.listen(port, (err) => {
     if(err){
         return console.log('Something went wrong', err)
     }
-    console.log('Server Listening on: ${port}');
+    console.log(`Server Listening on: ${port}`);
 });
